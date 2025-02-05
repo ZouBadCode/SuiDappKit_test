d095374761561237d86cf81384fd3dd14de0ba7f
zou <leftrightch@gmail.com>
2025-02-05 16:58:53 +0800

    use sui-dapp-kit in astro

---
+++ magenta-main/src/components/WalletProviderReact.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createNetworkConfig, SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConnectButton } from '@mysten/dapp-kit';
+
+
+const { networkConfig } = createNetworkConfig({
+	localnet: { url: getFullnodeUrl('localnet') },
+	mainnet: { url: getFullnodeUrl('mainnet') },
+});
+const queryClient = new QueryClient();
+
+const WalletProviderReact = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+    <SuiClientProvider networks={networkConfig} defaultNetwork="localnet">
+      <WalletProvider>
+        <ConnectButton/>
+      </WalletProvider>
+    </SuiClientProvider>
+  </QueryClientProvider>
+  );
+};
+
+export default WalletProviderReact;
